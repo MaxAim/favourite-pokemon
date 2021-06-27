a17974711681cc37588803ba6cb8aec9735381b0
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-06-27 06:25:18 -0300

    Add files via upload

--- Vote.js
+++ Vote.js
@@ -16,13 +16,17 @@ const Load = () => {
         pokemon1 = Math.floor(Math.random() * pokemonList.length);
         pokemon2 = Math.floor(Math.random() * pokemonList.length);
         if(pokemon1 !== pokemon2){
+            $("h2:eq(0)").hide();
+            $("img:eq(0)").hide();
+            $("h2:eq(1)").hide();
+            $("img:eq(1)").hide();
             $.ajax({
                 type: "get",
                 url: `https://pokeapi.co/api/v2/pokemon/${pokemonList[pokemon1]}`,
                 data: "data",
                 dataType: "JSON",
                 success: function (response) {
-                    $("h2:eq(0)").text(response.name)
+                    $("h2:eq(0)").text(response.name);
                     $("img:eq(0)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[pokemon1]}.png`);
                 }
             });
@@ -32,8 +36,14 @@ const Load = () => {
                 data: "data",
                 dataType: "JSON",
                 success: function (response) {
-                    $("h2:eq(1)").text(response.name)
+                    $("h2:eq(1)").text(response.name);
                     $("img:eq(1)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[pokemon2]}.png`);
+                    $("h2:eq(0)").show();
+                    $("h2:eq(1)").show();
+                    setTimeout(() => {
+                        $("img:eq(0)").fadeIn(500);
+                        $("img:eq(1)").fadeIn(500);
+                    }, 600);
                 }
             });
         }
@@ -51,8 +61,7 @@ const Load = () => {
                 $("h1").text("Your favourite pokemon is:");
                 $("h2:eq(0)").text(response.name);
                 $("img:eq(0)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[0]}.png`);
-                $("h2:eq(1)").hide();
-                $("img:eq(1)").hide();
+                $(".subContainer:eq(1)").hide();
                 $(".mainContainer").css("width", "0");
             }
         });
