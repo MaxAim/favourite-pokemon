df523f73bc5c2171c64b1174927fb57cf957bb11
MaxAim <75140148+MaxAim@users.noreply.github.com>
2021-06-27 23:11:55 -0300

    Add files via upload

--- Vote.js
+++ Vote.js
@@ -1,19 +1,69 @@
 var pokemonList = []
-var pokemon1 = 0
-var pokemon2 = 0
+var tempList = []
+var type = ""
 
-
-const CreateList = (i, gen) => {
-    while (i < gen){
-        i++
-        pokemonList.push(i)
+const CreateList = (i, gen, newType) => {
+    $("a:eq(0)").attr("onclick", "Vote(pokemon1)")
+    $("a:eq(1)").attr("onclick", "Vote(pokemon2)")
+    type = newType
+    if(type === ""){
+        while (i < gen){
+            i++
+            $.ajax({
+                type: "get",
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`,
+                data: "data",
+                dataType: "JSON",
+                success: function (response) {
+                    pokemonList.push(response.name)
+                }
+            })
+        }
     }
-    Load()
-    $("button").hide();
-    $("h1").text("Pick your favourite");
-    $(".subContainer").css("min-width", "37vw");
-    $(".subContainer").css("min-height", "43vw");
-    $(".desktop").css("display", "block");
+    else{
+        $.ajax({
+            type: "get",
+            url: `https://pokeapi.co/api/v2/type/${type}`,
+            data: "data",
+            dataType: "JSON",
+            success: function (response) {
+                object = response.pokemon
+                for (const key in object) {
+                    if (Object.hasOwnProperty.call(object, key)) {
+                        const element = object[key];
+                        pokemonList.push(element.pokemon.name)
+                    }
+                }
+            }
+        });
+    }
+    setTimeout(() => {
+        Load()
+        $("button").hide();
+        $("h1").text("Pick your favourite");
+        $(".subContainer").css("min-width", "37vw");
+        $(".subContainer").css("min-height", "43vw");
+        $(".desktop").css("display", "block");
+    }, 1000);
+}
+
+const AjaxPk = (pk, pkm) => {
+    $.ajax({
+        type: "get",
+        url: `https://pokeapi.co/api/v2/pokemon/${pokemonList[pk]}`,
+        data: "data",
+        dataType: "JSON",
+        success: function (response) {
+            $(`h2:eq(${pkm})`).hide();
+            $(`img:eq(${pkm})`).hide()
+            $(`h2:eq(${pkm})`).text(response.name);
+            $(`img:eq(${pkm})`).attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.id}.png`);
+            $(`h2:eq(${pkm})`).show();
+            setTimeout(() => {
+                $(`img:eq(${pkm})`).fadeIn(500);
+            }, 600);
+        }
+    });
 }
 
 
@@ -21,65 +71,32 @@ const Load = () => {
     if (pokemonList.length > 1){
         pokemon1 = Math.floor(Math.random() * pokemonList.length);
         pokemon2 = Math.floor(Math.random() * pokemonList.length);
-        if(pokemon1 !== pokemon2){
-            $("h2:eq(0)").hide();
-            $("img:eq(0)").hide();
-            $("h2:eq(1)").hide();
-            $("img:eq(1)").hide();
-            $.ajax({
-                type: "get",
-                url: `https://pokeapi.co/api/v2/pokemon/${pokemonList[pokemon1]}`,
-                data: "data",
-                dataType: "JSON",
-                success: function (response) {
-                    $("h2:eq(0)").text(response.name);
-                    $("img:eq(0)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[pokemon1]}.png`);
-                }
-            });
-            $.ajax({
-                type: "get",
-                url: `https://pokeapi.co/api/v2/pokemon/${pokemonList[pokemon2]}`,
-                data: "data",
-                dataType: "JSON",
-                success: function (response) {
-                    $("h2:eq(1)").text(response.name);
-                    $("img:eq(1)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[pokemon2]}.png`);
-                    $("h2:eq(0)").show();
-                    $("h2:eq(1)").show();
-                    setTimeout(() => {
-                        $("img:eq(0)").fadeIn(500);
-                        $("img:eq(1)").fadeIn(500);
-                    }, 600);
-                }
-            });
+        if (pokemon1 !== pokemon2){
+            AjaxPk(pokemon1, 0)
+            AjaxPk(pokemon2 ,1)
         }
         else{
-            Load()
+            Load(type)
         }
     }
     else{
-        $.ajax({
-            type: "get",
-            url: `https://pokeapi.co/api/v2/pokemon/${pokemonList[0]}`,
-            data: "data",
-            dataType: "JSON",
-            success: function (response) {
-                $("h1").text("Your favourite pokemon is:");
-                $("h2:eq(0)").text(response.name);
-                $("img:eq(0)").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonList[0]}.png`);
-                $(".subContainer:eq(1)").hide();
-                $(".mainContainer").css("width", "100%");
-            }
-        });
-    }
-}
+        AjaxPk(pokemonList[0]);
+        $("h1").text("Your favourite pokemon is:");
+        $(".subContainer:eq(1)").hide();
+        $(".mainContainer").css("width", "100%");
+    };
+};
 
 const Vote = (pokemonNum) => {
     pokemonList.splice(pokemonNum, 1)
     Load()
 }
 
-
-$(document).ready(function(){
-    Load()
-});
+const byGen = () => {
+    $(".genContainer").css("display", "inline")
+    $(".typeContainer").css("display", "none")
+}
+const byType = () => {
+    $(".typeContainer").css("display", "inline")
+    $(".genContainer").css("display", "none")
+}
